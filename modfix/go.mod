aa15300bef2bb1af6af95b517c96d9a8b2b748e3
Joel Hendrix <jhendrix@microsoft.com>
2020-06-23 09:53:38 -0700

    breaking change

--- modfix/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/jhendrixMSFT/go-modfix/modfix
-
-go 1.14
--- modfix/modfix.go
+++ modfix/modfix.go
@@ -3,6 +3,6 @@ package modfix
 import "fmt"
 
 // DoIt is a simple test function.
-func DoIt(i int) string {
-	return fmt.Sprintf("this is a test %d\n", i)
+func DoIt(s string) string {
+	return fmt.Sprintf("this is a test %s\n", s)
 }
